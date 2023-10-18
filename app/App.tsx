4706fec6a2198c044760e4ee6b5d4a1a6b0d965f
fullzer4 <gabrielpelizzaro@gmail.com>
2023-10-18 09:56:54 -0300

    base

---
+++ app/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = (): JSX.Element => {
+  return (
+    <View>
+      <Text> s </Text>
+    </View>
+  );
+};
+
+export default App;
